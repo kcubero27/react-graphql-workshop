8f6102a8dda7beb31a1a0d016e23cfe820fac4d1
Karen Cubero <kaaerreeene@gmail.com>
2019-02-26 15:52:50 +0100

    feat(apollo): add apollo configuration

--- src/components/app/app.component.tsx
+++ src/components/app/app.component.tsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import { ApolloProvider } from "react-apollo";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { apolloClient } from "../../apollo-client";
 import { ErrorBoundary } from "../error-boundary";
 
 export class App extends Component {
@@ -7,17 +9,19 @@ export class App extends Component {
         return (
             <BrowserRouter>
                 <ErrorBoundary>
-                    <Switch>
-                        <Route exact path="/">
-                            <h2>Landing</h2>
-                        </Route>
-                        <Route path="/create">
-                            <h2>Create</h2>
-                        </Route>
-                        <Route path="/post/:id">
-                            <h2>Post profile</h2>
-                        </Route>
-                    </Switch>
+                    <ApolloProvider client={apolloClient}>
+                        <Switch>
+                            <Route exact path="/">
+                                <h2>Landing</h2>
+                            </Route>
+                            <Route path="/create">
+                                <h2>Create</h2>
+                            </Route>
+                            <Route path="/post/:id">
+                                <h2>Post profile</h2>
+                            </Route>
+                        </Switch>
+                    </ApolloProvider>
                 </ErrorBoundary>
             </BrowserRouter>
         );
