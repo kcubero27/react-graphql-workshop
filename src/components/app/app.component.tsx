108dab9bcdf314f142a7f1fc2609b7cbac7e8722
Karen Cubero <kaaerreeene@gmail.com>
2019-02-26 15:10:34 +0100

    feat(error boundary): add an error boundary component

--- src/components/app/app.component.tsx
+++ src/components/app/app.component.tsx
@@ -1,21 +1,24 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { ErrorBoundary } from "../error-boundary";
 
 export class App extends Component {
     render() {
         return (
             <BrowserRouter>
-                <Switch>
-                    <Route exact path="/">
-                        <h2>Landing</h2>
-                    </Route>
-                    <Route path="/create">
-                        <h2>Create</h2>
-                    </Route>
-                    <Route path="/post/:id">
-                        <h2>Post profile</h2>
-                    </Route>
-                </Switch>
+                <ErrorBoundary>
+                    <Switch>
+                        <Route exact path="/">
+                            <h2>Landing</h2>
+                        </Route>
+                        <Route path="/create">
+                            <h2>Create</h2>
+                        </Route>
+                        <Route path="/post/:id">
+                            <h2>Post profile</h2>
+                        </Route>
+                    </Switch>
+                </ErrorBoundary>
             </BrowserRouter>
         );
     }
